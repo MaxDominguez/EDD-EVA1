0da0e751a377d6302ae7c1bc50ca358f9ca524a2
MaxDominguez <l22550359@chihuahua2.tecnm.mx>
2024-08-28 18:50:09 -0600

    Add files via upload

---
+++ EVA1_6_ARREGLOS.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class EVA1_6_ARREGLOS {
+    public static void main(String[] args) {
+       int[] arreglo = new int[10];
+       System.out.println(arreglo);
+       arreglo[0] = 100;
+        arreglo[1] = 200;
+        arreglo[2] = 300;
+        arreglo[3] = 400;
+        arreglo[4] = 500;
+        arreglo[5] = 600;
+        arreglo[6] = 700;
+        arreglo[7] = 800;
+        arreglo[8] = 900;
+       arreglo[9] = 1000;
+       System.out.println("arreglo 0 =" + arreglo[0]);
+        System.out.println("arreglo 9 =" + arreglo[9]);
+    }
+}
